66f6fc7f5e659e4731b8fc4100383804ff62de5e
pranavmohantyy <mohantypranav147@gmail.com>
2026-08-11 01:41:58 -0700

    add message history

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ rooms = defaultdict(list)
 class ConnectionManager:
     def __init__(self):
         self.active_connections = defaultdict(list)
+        self.message_history = defaultdict(list)
 
     async def connect(self, room_id: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections[room_id].append(websocket)
         self.broadcast(room_id, f"{websocket.client} joined")
+        await self.send_history(room_id, websocket)
 
     def disconnect(self, room_id: str, websocket: WebSocket):
         self.active_connections[room_id].remove(websocket)
@@ -22,9 +24,17 @@ class ConnectionManager:
     def broadcast(self, room_id: str, message: str):
         for connection in self.active_connections[room_id]:
             connection.send_text(message)
+        self.store_message(room_id, message)
 
-    def get_connections(self, room_id: str):
-        return self.active_connections[room_id]
+    def store_message(self, room_id: str, message: str):
+        self.message_history[room_id].append(message)
+        if len(self.message_history[room_id]) > 50:
+            self.message_history[room_id].pop(0)
+
+    async def send_history(self, room_id: str, websocket: WebSocket):
+        history = self.message_history[room_id]
+        for message in history:
+            await websocket.send_text(message)
 
 manager = ConnectionManager()
 
@@ -39,5 +49,5 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
         manager.disconnect(room_id, websocket)
 
 @app.get("/")
-async def get():
-    return HTMLResponse('<html><body><h1>WebSocket Chat</h1></body></html>')
+def get():
+    return HTMLResponse("<html><body><h1>WebSocket Chat</h1></body></html>")
